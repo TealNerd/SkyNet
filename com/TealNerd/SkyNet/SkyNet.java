63d32c79487c7d50c204504d46ed1b32f7e1d09a
biggestnerd <TealNerd@users.noreply.github.com>
2015-05-16 16:12:56 -0500

    1.8 update

--- com/TealNerd/SkyNet/SkyNet.java
+++ com/TealNerd/SkyNet/SkyNet.java
@@ -1,64 +1,41 @@
-package com.TealNerd.SkyNet;
+package com.biggestnerd.skynet;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 import org.lwjgl.input.Keyboard;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
-//import cpw.mods.fml.common.Mod.PreInit;    // used in 1.5.2
-//import cpw.mods.fml.common.Mod.Init;       // used in 1.5.2
-//import cpw.mods.fml.common.Mod.PostInit;   // used in 1.5.2
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-
-@Mod(modid="SkyNet", name="SkyNet", version="1.0.0")
-
+@Mod(modid="skynet", name="SkyNet", version="v1.1.0")
 public class SkyNet {
-
+	
 	static Minecraft mc = Minecraft.getMinecraft();
 	static boolean isEnabled = true;
 	public static KeyBinding toggle;
 	static List<String> previousPlayerList = new ArrayList();
-	
-    // The instance of your mod that Forge uses.
-    @Instance(value = "SkyNet")
-    public static SkyNet instance;
-    
-    @SidedProxy(clientSide="com.TealNerd.SkyNet.CommonProxy")
-    public static CommonProxy proxy;
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)  {
-    	SkyNet.instance = this;
-    	FMLCommonHandler.instance().bus().register(new KeyInputHandler());
-    }
-    
-    
-    
-    @EventHandler
-    public void load(FMLInitializationEvent event) {
-            proxy.registerListeners();
-            TickHandler handler = new TickHandler();
-            FMLCommonHandler.instance().bus().register(handler);
-            toggle = new KeyBinding("Toggle SkyNet", Keyboard.KEY_I, "SkyNet");
-            ClientRegistry.registerKeyBinding(toggle);           
-    }
-    
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
+    	FMLCommonHandler.instance().bus().register(this);
+    	MinecraftForge.EVENT_BUS.register(this);
+    	toggle = new KeyBinding("Toggle SkyNet", Keyboard.KEY_I, "SkyNet");
+        ClientRegistry.registerKeyBinding(toggle);     
     }
     
     public static String filterChatColors(String s) {
@@ -66,10 +43,51 @@ public class SkyNet {
     }
     
     public static void onPlayerLeave(String player) {
-    	mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.YELLOW + player + " left the game"));
+    	mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.GRAY + player + " left the game"));
     }
     
     public static void onPlayerJoin(String player) {
-    	mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.YELLOW + player + " joined the game"));
+    	mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.GRAY + player + " joined the game"));
+    }
+
+	@SubscribeEvent
+    public void onTick(ClientTickEvent event) {
+    	if(event.phase == TickEvent.Phase.START) {
+    		if(SkyNet.isEnabled) {
+    			if(mc.theWorld != null) { 		
+    			ArrayList<String> playerList = new ArrayList();
+    			Collection players = mc.getNetHandler().func_175106_d();
+    			for(Object o : players) {
+    				if((o instanceof NetworkPlayerInfo)) {
+    					NetworkPlayerInfo info = (NetworkPlayerInfo)o;
+    					playerList.add(SkyNet.filterChatColors(info.getGameProfile().getName()));
+    				}
+    			}
+    			ArrayList<String> temp = (ArrayList)playerList.clone();
+    			playerList.removeAll(SkyNet.previousPlayerList);
+    			SkyNet.previousPlayerList.removeAll(temp);
+    			for(String player : SkyNet.previousPlayerList) {
+    				SkyNet.onPlayerLeave(player);
+    			}
+    			for(String player : playerList) {
+    				SkyNet.onPlayerJoin(player);
+    			}
+    			SkyNet.previousPlayerList = temp;
+    			}
+    		}
+    	}
+    }
+	
+	@SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        if(SkyNet.toggle.isPressed()){
+        	if(!SkyNet.isEnabled){
+        	mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.GRAY + "SkyNet Enabled"));
+        	SkyNet.isEnabled = true;
+        	}else if(SkyNet.isEnabled){
+        	mc.ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[SkyNet] "+ EnumChatFormatting.GRAY + "SkyNet Disabled"));
+        	SkyNet.isEnabled = false;
+        	}
+        }              
     }
 }
